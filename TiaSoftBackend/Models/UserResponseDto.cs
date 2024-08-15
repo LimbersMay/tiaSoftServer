eeb95406fec7fbe88f28ff32ee14925939fde169
LimbersMay <limbermay1@gmail.com>
2024-08-15 17:31:39 -0600

    Propiedad UserName remplazada por Username

--- TiaSoftBackend/Models/UserResponseDto.cs
+++ TiaSoftBackend/Models/UserResponseDto.cs
@@ -2,7 +2,7 @@ namespace TiaSoftBackend.Models;
 
 public class UserResponseDto
 {
-    public string UserName { get; set; }
+    public string Username { get; set; }
     public string Email { get; set; }
     public List<string> Roles { get; set; }
 }
