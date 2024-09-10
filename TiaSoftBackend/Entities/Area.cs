f897554ed2c3b3918eb743bb087106128552f161
LimbersMay <limbermay1@gmail.com>
2024-09-09 21:05:30 -0600

    Entidad de área creado para representar un área en la base de datos

---
+++ TiaSoftBackend/Entities/Area.cs
@@ -0,0 +1,8 @@
+namespace TiaSoftBackend.Entities;
+
+public class Area
+{
+    public string AreaId { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+}
