804a4661afff2b17d6f60dea233db00636810086
LimbersMay <limbermay1@gmail.com>
2024-10-03 11:20:38 -0600

    Data Annotation de MaxLength agregado al Name, Description y Icon

--- TiaSoftBackend/Entities/Category.cs
+++ TiaSoftBackend/Entities/Category.cs
@@ -10,8 +10,14 @@ public class Category
     public string CategoryId { get; set; }
     
     [Required]
+    [MaxLength(36)]
     [DataType(DataType.Text)]
     public string Name { get; set; }
     
+    [MaxLength(255)]
     public string? Description { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string Icon { get; set; }
 }
