72a09dabbf8260a0583fb6b15337c1713f4f615a
LimbersMay <limbermay1@gmail.com>
2024-11-17 15:03:35 -0600

    Migraciones registradas

---
+++ TiaSoftBackend/Migrations/20241009021806_BillStatusRemoved.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiaSoftBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class BillStatusRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bills_BillStatuses_BillStatusId",
+                table: "Bills");
+
+            migrationBuilder.DropTable(
+                name: "BillStatuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bills_BillStatusId",
+                table: "Bills");
+
+            migrationBuilder.DropColumn(
+                name: "BillStatusId",
+                table: "Bills");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BillStatusId",
+                table: "Bills",
+                type: "varchar(36)",
+                maxLength: 36,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "BillStatuses",
+                columns: table => new
+                {
+                    BillStatusId = table.Column<string>(type: "varchar(36)", maxLength: 36, nullable: false),
+                    Description = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Status = table.Column<string>(type: "varchar(36)", maxLength: 36, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BillStatuses", x => x.BillStatusId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bills_BillStatusId",
+                table: "Bills",
+                column: "BillStatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bills_BillStatuses_BillStatusId",
+                table: "Bills",
+                column: "BillStatusId",
+                principalTable: "BillStatuses",
+                principalColumn: "BillStatusId");
+        }
+    }
+}
---
+++ TiaSoftBackend/Migrations/20241113223830_OrderNumberAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiaSoftBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderNumberAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderNumber",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderNumber",
+                table: "Orders");
+        }
+    }
+}
