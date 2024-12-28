7141fea6247ef4e5987d3f760a450c5b3ad7faa2
LimbersMay <limbermay1@gmail.com>
2024-12-27 20:28:59 -0600

    Atributo Product reemplazado por ProductName en OrderProductDto

--- TiaSoftBackend.DTOs/Orders/OrderProductDto.cs
+++ TiaSoftBackend.DTOs/Orders/OrderProductDto.cs
@@ -7,7 +7,7 @@ public class OrderProductDto
     public required string OrderId { get; set; }
     public required string ProductId { get; set; }
     
-    public required MenuDto Product { get; set; }
+    public required string ProductName { get; set; }
     public int Quantity { get; set; }
     
     public decimal PriceAtOrder { get; set; }
